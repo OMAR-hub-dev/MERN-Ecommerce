653957fb97dccc435bf8af91e1a5ac41fc2bf1b2
o.omar <oulaya.omar@gmail.com>
2022-03-05 13:12:01 +0100

    dépendance

---
+++ frontend/src/App.js
@@ -0,0 +1,25 @@
+
+import './App.css';
+
+import {BrowserRouter, Link, Route, Routes} from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+
+function App() {
+  return (
+    <BrowserRouter>
+    <div className="App">
+      <Link to ="/">Attire</Link>
+      <main>
+        <Routes>
+          <Route path="/product/:slug" element={<ProductScreen/>}/>
+          <Route path="/" element={<HomeScreen/>}/>
+        </Routes>
+                 
+      </main>
+    </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ frontend/src/screens/ProductScreen.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { useParams } from 'react-router-dom'
+
+const ProductScreen = () => {
+
+
+    const {slug} = useParams();
+    
+  return (
+    <div>
+        <h1>{slug}</h1>
+    </div>
+  )
+}
+
+export default ProductScreen
