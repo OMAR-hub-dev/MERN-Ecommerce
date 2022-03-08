e7705bfd5aa8ba667b2b448a4b714c252742ddbf
o.omar <oulaya.omar@gmail.com>
2022-03-08 12:44:35 +0100

    react-bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,26 @@
 
 import './App.css';
 
-import {BrowserRouter, Link, Route, Routes} from 'react-router-dom';
+import {BrowserRouter, Link, NavLink, Route, Routes} from 'react-router-dom';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import {LinkContainer} from 'react-router-bootstrap';
 
 function App() {
   return (
     <BrowserRouter>
-    <div className="App">
-      <Link to ="/">Attire</Link>
+    <div className="App d-flex flex-column site-container">
+      <header>
+        <Navbar bg="warning" variant="dark">
+          <Container>
+            <LinkContainer to="/">
+              <Navbar.Brand>Bondoufle store</Navbar.Brand>
+            </LinkContainer>
+          </Container>
+        </Navbar>
+      </header>
       <main>
         <Routes>
           <Route path="/product/:slug" element={<ProductScreen/>}/>
@@ -17,6 +28,9 @@ function App() {
         </Routes>
                  
       </main>
+      <footer>
+        <div className='text-center'> ALL Rights Reserverd </div>
+      </footer>
     </div>
     </BrowserRouter>
   );
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,15 +1,41 @@
-import React, { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom'
-import data from '../data'
+import React, { useEffect, useReducer, useState } from 'react'
+//  import data from '../data'
 import axios from 'axios'
+import logger from 'use-reducer-logger';
+import Product from '../componenents/Product';
+
+// declarion de reducer
+const reducer = (state, action )=>{
+  switch (action.type){
+    case 'FETCH_REQUEST':
+      return {...state, loading : true};
+      
+    case 'FETCH_SUCCESS':
+      return {...state,products:action.payload, loading : false};
+    case 'FETCH_FAIL':
+      return {...state, loading : false};
+      default: return state;
+
+  }
+};
 
 
 const HomeScreen = () => {
-  const [products, setProducts] = useState([]);
+ // on remplace le hooks qu'on a utiliser  par reducer## const [products, setProducts] = useState([]);
+//  le reducer prend 2 paramettre 1er c' le objet, 2eme c la fonction dispatch , use Reducer prend egalement 2 paramette est le state par default
+ const [{loading, error, products}, dispatch] = useReducer(logger(reducer), {loading:true, error:'',products:[] });
+  // recuperer la data du backend
   useEffect(() => {
     const fetchData = async()=>{
+      dispatch({type:'FETCH_REQUEST' })
+      try{
       const result = await axios.get('/api/products');
-      setProducts(result.data);
+      dispatch({type:'FETCH_SUCCESS', payload:result.data })
+
+      }catch{
+        dispatch({type:'FETCH_FAIL', payload: error.message})
+      }
+      // setProducts(result.data);
     };
   fetchData();
   }, [])
@@ -19,16 +45,10 @@ const HomeScreen = () => {
     <div>
          <h1>Featured Products</h1>
           <div className='products'>
-            {data.products.map(product => (<div  className='product' key={product.slug}>
-                
-                <Link to ={`/product/${product.slug}`}><img src={product.image} alt={product.name} /></Link>
-                <div className='product-info'>
-                <Link to ={`/product/${product.slug}`}><p>{product.name}</p></Link>
-                  <strong><p>{product.price}</p></strong>
-                  <button>Add to cart</button>
-                </div>
-                
-          </div>))}
+            {
+            loading ? (<div>...Loading </div>)
+            : error ? (<div>{error}</div>)
+            : products.map(product => (<Product product= {product} ></Product>))}
           </div>
     </div>
   )
