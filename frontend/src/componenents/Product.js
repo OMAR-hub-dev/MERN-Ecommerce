00ab8519d5fb064829b70c1767685215671622f3
o.omar <oulaya.omar@gmail.com>
2022-03-28 12:27:24 +0200

    SigninScreen

--- frontend/src/componenents/Product.js
+++ frontend/src/componenents/Product.js
@@ -1,7 +1,6 @@
 
 import axios from 'axios';
 import React, { useContext } from 'react';
-import Button from 'react-bootstrap/Button';
 import { Link } from 'react-router-dom';
 import { Store } from '../Store';
 import Rating from './Rating';
